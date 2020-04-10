06a2278559340a2db855e395268792655c255df6
Muzhe Zeng <mzeng6@wisc.edu>
2020-04-09 22:53:45 -0500

    zzz .R file

---
+++ R/zzz.R
@@ -0,0 +1,20 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Welcome to Muzhe's package!")
+}
+
+.onLoad <- function(libname, pkgname) {
+  op <- options()
+  op.devtools <- list(
+    devtools.path = "~/R-dev",
+    devtools.install.args = "",
+    devtools.name = "Muzhe Zeng",
+    devtools.desc.author = "Muzhe Zeng <mzeng6@wisc.edu> [aut, cre]",
+    devtools.desc.license = "What license is it under?",
+    devtools.desc.suggests = NULL,
+    devtools.desc = list()
+  )
+  toset <- !(names(op.devtools) %in% names(op))
+  if(any(toset)) options(op.devtools[toset])
+
+  invisible()
+}
